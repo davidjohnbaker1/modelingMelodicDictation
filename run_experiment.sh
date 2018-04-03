83208c9e1f2032c5318ca1b9e8a7131abc3bebd7
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2018-04-03 18:07:08 -0500

    added experiment run script

---
+++ run_experiment.sh
@@ -0,0 +1,3 @@
+open https://docs.google.com/spreadsheets/d/1ksojHgJOo3vPdJuogysT_MaVLgElw_KEXfkPhoWqIk0/edit?usp=sharing # opens course credit
+open https://docs.google.com/spreadsheets/d/1RXVRH5NaF7KqEqDPntay6l_ay6JdCMiOTwk60Wu1TvQ/edit?usp=sharing # opens dictation participant log
+open experiment/experiment.html -a Firefox # Opens Experiment
